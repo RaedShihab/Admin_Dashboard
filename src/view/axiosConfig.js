c477f6ac1a6ef9e6d20454b5fa3281a686a5c60b
RaedShihab <raedshihab92@gmail.com>
2020-02-25 13:04:52 +0200

    starting online

--- src/view/axiosConfig.js
+++ src/view/axiosConfig.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 const token = ()=> {
     return localStorage.getItem('user')
   }
+  let parcedToken
+  if(JSON.parse(token())!==null) {
+    parcedToken = JSON.parse(token()).token
+  }
   
 export const Axios = axios.create({
     baseURL: 'https://api.glowyhan.com/gateway',
     headers: {
       'Accept': 'application/json',
       'Content-Type' : 'multipart/form-data',
-      'Authorization': 'Bearer ' + JSON.parse(token()).token
+      'Authorization': 'Bearer ' + parcedToken
   }
   });
   
